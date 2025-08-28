4634d001506282c18d8c76c154ffe61ace7d86e1
RensVanGiersbergen <rensgiersbergen@gmail.com>
2025-08-28 14:44:32 +0200

    Add contact form and controller for contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request): Response
+    {
+        // DRY
+        $notBlank = new NotBlank([
+            'message' => 'This field cannot be empty',
+        ]);
+
+        // Build the form
+        $form = $this->createFormBuilder()
+            // Regex pattern: [a-zA-Z\s] to allow only letters and spaces
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'attr' => ['class' => 'form-control', 'pattern' => '[a-zA-Z\s]+', 'title' => 'Only letters and spaces are allowed'],
+                'constraints' => [
+                    $notBlank,
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z\s]+$/',
+                        'message' => 'Name can only contain letters and spaces',
+                    ]),
+                ],
+            ])
+            ->add('topic', TextType::class, [
+                'label' => 'Topic',
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    $notBlank,
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => ['class' => 'form-control', 'rows' => 6],
+                'constraints' => [
+                    $notBlank,
+                ],
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'Send Message',
+                'attr' => ['class' => 'btn btn-primary mt-3'],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        // Process the form submission
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $this->addFlash('success', sprintf(
+                'Thanks %s! Your message about "%s" was received',
+                $data['name'],
+                $data['topic']
+            ));
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'contactForm' => $form->createView(),
+        ]);
+    }
+}
